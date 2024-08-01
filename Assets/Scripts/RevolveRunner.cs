098464cebeed4d22dd7fef47fe1f98110da9965a
donalshortt <donalshortt2000@gmail.com>
2024-08-01 12:50:43 +0200

    added gitignore & latest

--- Assets/Scripts/RevolveRunner.cs
+++ Assets/Scripts/RevolveRunner.cs
@@ -2,98 +2,103 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEditor.Scripting.Python;
 using Mujoco;
 using System.Text;
 
+using Debug = UnityEngine.Debug;
+
 public static class JsonHelper
 {
-    [Serializable]
-    private class Wrapper<T>
-    {
-        public T[] Items;
-    }
+	[Serializable]
+	private class Wrapper<T>
+	{
+		public T[] Items;
+	}
 
-    public static T[] FromJson<T>(string json)
-    {
-        string newJson = "{\"Items\":" + json + "}";
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
-        return wrapper.Items;
-    }
+	public static T[] FromJson<T>(string json)
+	{
+		string newJson = "{\"Items\":" + json + "}";
+		Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+		return wrapper.Items;
+	}
 
-    public static string ToJson<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T> { Items = array };
-        return JsonUtility.ToJson(wrapper);
-    }
+	public static string ToJson<T>(T[] array)
+	{
+		Wrapper<T> wrapper = new Wrapper<T> { Items = array };
+		return JsonUtility.ToJson(wrapper);
+	}
 }
 
 [System.Serializable]
 public class SimulationSceneState
 {
-    public float[] xpos;
-    public float[] xquat;
-    public float[] qpos;
+	public float[] xpos;
+	public float[] xquat;
+	public float[] qpos;
 
-    public override string ToString()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("SimulationSceneState:");
+	public override string ToString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.AppendLine("SimulationSceneState:");
 
-        sb.Append("xpos: ");
-        if (qpos != null)
-        {
-            sb.Append(string.Join(", ", xpos));
-        }
-        else
-        {
-            sb.Append("null");
-        }
-        sb.AppendLine();
+		sb.Append("xpos: ");
+		if (qpos != null)
+		{
+			sb.Append(string.Join(", ", xpos));
+		}
+		else
+		{
+			sb.Append("null");
+		}
+		sb.AppendLine();
 
-        sb.Append("xquat: ");
-        if (qpos != null)
-        {
-            sb.Append(string.Join(", ", xquat));
-        }
-        else
-        {
-            sb.Append("null");
-        }
-        sb.AppendLine();
+		sb.Append("xquat: ");
+		if (qpos != null)
+		{
+			sb.Append(string.Join(", ", xquat));
+		}
+		else
+		{
+			sb.Append("null");
+		}
+		sb.AppendLine();
 
-        sb.Append("qpos: ");
-        if (qpos != null)
-        {
-            sb.Append(string.Join(", ", qpos));
-        }
-        else
-        {
-            sb.Append("null");
-        }
-        sb.AppendLine();
+		sb.Append("qpos: ");
+		if (qpos != null)
+		{
+			sb.Append(string.Join(", ", qpos));
+		}
+		else
+		{
+			sb.Append("null");
+		}
+		sb.AppendLine();
 
-        return sb.ToString();
-    }
+		return sb.ToString();
+	}
 }
 
 [System.Serializable]
 public class SimulationScene
 {
-    public SimulationSceneState[] scenes;
+	public SimulationSceneState[] scenes;
 }
 
 public class RevolveRunner : MonoBehaviour
 {
-    private SimulationScene simulationScene;
-    private int currentIndex = 0;
+	private SimulationScene simulationScene;
+	private int currentIndex = 0;
 
-    void ImportScene()
-    {
-        string path = $"{Application.dataPath}/model.xml";
-        var importer = new MjImporterWithAssets();
-        importer.ImportFile(path);
+	GameObject ImportScene()
+	{
+		string path = $"{Application.dataPath}/model.xml";
+		var importer = new MjImporterWithAssets();
+		GameObject importedScene = importer.ImportFile(path);
+
+		return importedScene;
 	}
 
 	void ApplyShaders()
@@ -133,15 +138,67 @@ public class RevolveRunner : MonoBehaviour
 		currentIndex++;
 	}
 
+	// Example method to run the Python command
+	public void RunRevolve()
+	{
+		string command = "py -3.11 Assets/revolve2/vr/main.py";  // Your command
+
+		// Initialize the ProcessStartInfo
+		ProcessStartInfo processInfo = new ProcessStartInfo
+		{
+			FileName = "cmd.exe",   // Use cmd.exe for Windows
+			Arguments = $"/c {command}",
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+			UseShellExecute = false,
+			CreateNoWindow = true
+		};
+
+		// Start the process
+		using (Process process = Process.Start(processInfo))
+		{
+			// Read the output
+			string output = process.StandardOutput.ReadToEnd();
+			string error = process.StandardError.ReadToEnd();
+
+			process.WaitForExit();
+
+			// Log the output (or do something with it)
+			Debug.Log("Output: " + output);
+			if (!string.IsNullOrEmpty(error))
+			{
+				Debug.Log("Error: " + error);
+			}
+		}
+	}
+
+	void SetScenePosition(GameObject sceneRoot, Vector3 position)
+	{
+		if (sceneRoot != null)
+		{
+			sceneRoot.transform.position = position;
+		}
+		else
+		{
+			Debug.LogError("Imported scene root is null.");
+		}
+	}
+
 	void Start()
 	{
 		Application.targetFrameRate = 60;
-		Time.timeScale = 0.5f;
+		Time.timeScale = 1;
 		Debug.Log("Running python script");
 
-		//PythonRunner.RunFile($"{Application.dataPath}/Scripts/RevolveEntry.py");
+		//PythonRunner.RunFile($"{Application.dataPath}/revolve2/vr/main.py");
+		RunRevolve();
 
-		ImportScene();
+		GameObject mujocoScene1 = ImportScene();
+		GameObject mujocoScene2 = ImportScene();
+
+		SetScenePosition(mujocoScene1, new Vector3(10f, 0f, 5f));
+		SetScenePosition(mujocoScene2, new Vector3(-10f, 0f, -5f));
+
 		ApplyShaders();
 		MjScene.Instance.ctrlCallback += (_, _) => TrackMujocoData();
 
@@ -149,13 +206,8 @@ public class RevolveRunner : MonoBehaviour
 		filePath = filePath.Replace("\\", "/");
 		string jsonData = File.ReadAllText(filePath);
 
-		Debug.Log("Filepath: " + filePath);
-
 		SimulationSceneState[] sceneStates = JsonHelper.FromJson<SimulationSceneState>(jsonData);
 		simulationScene = new SimulationScene { scenes = sceneStates };
-
-		Debug.Log("SIMULATION DATA");
-		Debug.Log(simulationScene.scenes[0]);
 	}
 
 	void Update()
--- Assets/Scripts/RevolveViewerLoader.cs
+++ Assets/Scripts/RevolveViewerLoader.cs
@@ -1,12 +1,44 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneOnClick : MonoBehaviour
+public class RevolveViewerLoader : MonoBehaviour
 {
-    public string sceneName;
+	public string sceneName;
 
-    public void LoadScene()
-    {
-        //SceneManager.LoadScene(RevolveViewer);
-    }
+	public void LoadScene()
+	{
+		StartCoroutine(LoadSceneAdditively());
+	}
+
+	IEnumerator LoadSceneAdditively()
+	{
+		// Start loading the new scene additively
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+		// Disable scene activation until fully loaded
+		asyncLoad.allowSceneActivation = false;
+
+		// Wait until the scene is fully loaded
+		while (!asyncLoad.isDone)
+		{
+			// Check if the scene has been loaded to 90% (which means loading is essentially done)
+			if (asyncLoad.progress >= 0.9f)
+			{
+				// Optionally, display a loading screen or perform some task
+
+				// Allow the scene to activate
+				asyncLoad.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+
+		// At this point, the new scene is loaded and active
+
+		// Optionally, unload the previous scene (if not needed anymore)
+		// SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+
+		// Optionally, you can trigger some event, e.g., fade in the new scene, etc.
+	}
 }
